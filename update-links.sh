f39908bfadeeb9e4f7db98368026e963e8502434
Hipy Cas <hipycas@gmail.com>
2021-05-13 17:37:36 +0200

    Exclude README, picture and git related files from link creation

--- update-links.sh
+++ update-links.sh
@@ -1,5 +1,5 @@
 for file in `ls -a $HOME/dotfiles`; do
-	if [ $file != "update-links.sh" -a $file != "." -a $file != ".." ]; then	
+	if [ $file != "update-links.sh" -a $file != ".git" -a $file != ".gitignore" -a $file != "carbon.png" -a $file != "README.md" -a $file != "." -a $file != ".." ]; then	
 		if [ -e $HOME/$file ]; then 
       echo $HOME/dotfiles/$file is already linked to $HOME/$file
     else
